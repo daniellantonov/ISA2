3d7d847fba592444bd45a707a05dcda1f810221e
daniellantonov <daniellantonov@gmail.com>
2019-02-04 14:23:46 +0200

    911

---
+++ Test/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Test
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string volodja = "volodja";
+            Console.WriteLine(volodja);
+            Console.ReadLine();
+        }
+    }
+}
