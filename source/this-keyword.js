35b9fb238b1c31515889cb398c5caf842dae2c29
daniellantonov <daniellantonov@gmail.com>
2019-02-13 23:11:57 +0200

    this keyword

--- source/this-keyword.js
+++ source/this-keyword.js
@@ -26,7 +26,7 @@ console.log(first() === global); */
 console.log(second() === global);
 console.log(second() === undefined); */
 
-let myObject = { value: 'My Object' };
+/* let myObject = { value: 'My Object' };
 
 global.value = 'Global Object';
 
@@ -37,4 +37,24 @@ function third(name) {
 //console.log(third());
 
 //console.log(third.call(myObject));
-console.log(third.apply(myObject, [' bob']));
+console.log(third.apply(myObject, [' bob'])); */
+
+
+function fifth() {
+    console.log(this.firstName + ' ' + this.lastName);
+}
+
+let customer1 = {
+    firstName: 'Daniel',
+    lastName: 'Antonov',
+    print: fifth
+}
+
+let customer2 = {
+    firstName: 'Bob',
+    lastName: 'Tabor',
+    print: fifth
+}
+
+customer2.print();
+customer1.print();
