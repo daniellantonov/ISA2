4838f8be5d399896cbed338fbf52110a728dd202
daniellantonov <daniellantonov@gmail.com>
2019-02-13 16:00:57 +0200

    decisions

---
+++ source/decisions.js
@@ -0,0 +1,32 @@
+/*
+var count = 3;
+
+if (count == 4) {
+    console.log('Count is 4');
+} else if (count > 4) {
+    console.log('Count is greater than 4');
+} else if (count < 4) {
+    console.log('Count is less than 4');
+} else {
+    console.log('Count is not 4');
+}
+
+let hero = 'superman';
+switch (hero.toLocaleLowerCase()) {
+    case 'superman':
+        console.log('Super strenght');
+        console.log('x-ray vision');
+        break;
+    case 'batman':
+        console.log('intelligence');
+        console.log('fighting skills');
+        break;
+    default:
+        console.log('member og JLA');
+}
+*/
+
+let a = 1,
+    b = '1';
+let result = (a === b) ? 'equal' : 'ineuqal';
+console.log(result);
