98bd3c31ae4a3b158ceb4e52191775fa4f019426
daniellantonov <daniellantonov@gmail.com>
2019-02-24 15:27:20 +0200

    sa

--- Week 3 JavaScript/dom-intro.js
+++
@@ -1,8 +0,0 @@
-(function() {
-    function clickHandler(message) {
-        console.log('hi....' + message);
-    }
-
-    let myButton = document.getElementById('myButton');
-    myButton.addEventListener('click', function() { clickHandler('hi from iief') });
-})();
